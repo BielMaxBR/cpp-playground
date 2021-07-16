40d7cd8e8e5a3b078161b3ed5d8be3af9b653b3d
Gabriel Maximo <gbielmaximo@gmail.com>
2021-07-16 21:03:42 +0000

    mais um .h

--- include/tile.h
+++ include/tile.h
@@ -1,11 +1,14 @@
 #pragma once
 
+#include <vector2.h>
+
 class Tile {
     private: {
         float G;
         float H;
         float F;
         int neighbors[4];
+        int parent
     }
     public: {
 
---
+++ include/vector2.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Vector2 {
+    public: {
+        int x;
+        int y;
+        Vector(int x, int y);
+    }
+}
